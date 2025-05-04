42b48edea8ef5143751df9beed8a3152c070e019
Torinad2 <157666959+Torinad2@users.noreply.github.com>
2025-05-04 14:49:51 -0400

    Assignment 1 chapter 2 from book Starting Out with Java
    Name, Age, and Annual Income
    
    Write a program that declares the following:
    • a String variable named name
    • an int variable named age
    • a double variable named annualPay
    Store your age, name, and desired annual income as literals in these variables. The program
    should display these values on the screen in a manner similar to the following:
    My name is Joe Mahoney, my age is 26 and
    I hope to earn $100000.0 per year.

---
+++ src/NameAgeAndAnnualIncome.java
@@ -0,0 +1,28 @@
+/**
+ ***********************************************************************************************
+ * Assignment: Name, Age, and Annual Income                                                    *
+ *                                                                                             *
+ * Write a program that declares the following:                                                *
+ *                                                                                             *
+ * • a String variable named name                                                              *
+ * • an int variable named age                                                                 *
+ * • a double variable named annualPay                                                         *
+ *                                                                                             *
+ * Store your age, name, and desired annual income as literals in these variables. The program *
+ * should display these values on the screen in a manner similar to the following:             *
+ * My name is Joe Mahoney, my age is 26, and I hope to earn $100000.0 per year.                *
+ ***********************************************************************************************
+ */
+
+public class NameAgeAndAnnualIncome
+{
+    public static void main(String[] args)
+    {
+        String name = "Nikita Baiborodov";
+        int age = 43;
+        double annualPay = 300_000.0;
+
+        System.out.println("My name is " + name + ", my age is " + age + " and");
+        System.out.println("I hope to earn $" + annualPay + " per year.");
+    }
+}
